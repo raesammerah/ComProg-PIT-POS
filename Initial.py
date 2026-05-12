eb0855a15fbb00c71d10f23d4d53d6a0ee63735b
Rea Sammerah <piit.reasammerah03@gmail.com>
2026-05-12 14:58:53 +0800

    receipt and other added features

--- Initial.py
+++ Initial.py
@@ -131,5 +131,220 @@ def checkout():
             "No items in cart."
         )
         return
+total = 0
 
-    total = 0
+receipt = ""
+receipt += "================================\n"
+receipt += "       EVONNE ANGEL'S BURGER\n"
+receipt += f"{datetime.now()}\n"
+receipt += "--------------------------------\n"
+
+for item in cart:
+
+        subtotal = item["qty"] * item["price"]
+        total += subtotal
+
+        receipt += (
+            f"{item['name']} x{item['qty']}"
+            f" = ₱{subtotal}\n"
+        )
+
+receipt += "--------------------------------\n"
+receipt += f"TOTAL AMOUNT: ₱{total}\n"
+receipt += "================================\n"
+receipt += "      THANK YOU!\n"
+
+messagebox.showinfo("Receipt", receipt)
+
+cart.clear()
+update_cart()
+
+# HEADER
+header = tk.Label(
+    root,
+    text="Evonne Angel's Burger Point of Sale",
+    bg=BG,
+    fg=TEXT,
+    font=("Segoe UI", 24, "bold")
+)
+header.pack(pady=20)
+
+# MAIN FRAME
+main_frame = tk.Frame(root, bg=BG)
+main_frame.pack(fill="both", expand=True)
+
+# PRODUCTS FRAME
+products_frame = tk.Frame(main_frame, bg=BG)
+products_frame.pack(side="left", fill="both", expand=True, padx=20)
+
+
+# ORDER FRAME
+order_frame = tk.Frame(
+    main_frame,
+    bg=CARD,
+    width=420
+)
+order_frame.pack(
+    side="right",
+    fill="y",
+    padx=20,
+    pady=10
+)
+
+# ORDER TITLE
+order_title = tk.Label(
+    order_frame,
+    text="Customer Orders",
+    bg=CARD,
+    fg=TEXT,
+    font=("Segoe UI", 20, "bold")
+)
+order_title.pack(pady=20)
+
+
+# TABLE
+columns = ("Product", "Qty", "Price", "Subtotal")
+
+cart_table = ttk.Treeview(
+    order_frame,
+    columns=columns,
+    show="headings",
+    height=15
+)
+
+for col in columns:
+    cart_table.heading(col, text=col)
+    cart_table.column(col, width=90)
+
+cart_table.pack(padx=10)
+
+
+# TOTAL LABEL
+total_label = tk.Label(
+    order_frame,
+    text="TOTAL: ₱0",
+    bg=CARD,
+    fg=PRIMARY,
+    font=("Segoe UI", 22, "bold")
+)
+total_label.pack(pady=20)
+
+# BUTTONS
+
+button_frame = tk.Frame(order_frame, bg=CARD)
+button_frame.pack(pady=10)
+
+remove_btn = tk.Button(
+    button_frame,
+    text="Remove",
+    bg=RED,
+    fg="white",
+    relief="flat",
+    font=("Segoe UI", 11, "bold"),
+    padx=15,
+    pady=10,
+    cursor="hand2",
+    command=remove_item
+)
+remove_btn.grid(row=0, column=0, padx=5)
+
+clear_btn = tk.Button(
+    button_frame,
+    text="Clear Cart",
+    bg="#6B7280",
+    fg="white",
+    relief="flat",
+    font=("Segoe UI", 11, "bold"),
+    padx=15,
+    pady=10,
+    cursor="hand2",
+    command=clear_cart
+)
+clear_btn.grid(row=0, column=1, padx=5)
+
+checkout_btn = tk.Button(
+    order_frame,
+    text="Checkout",
+    bg=GREEN,
+    fg="white",
+    relief="flat",
+    font=("Segoe UI", 14, "bold"),
+    padx=30,
+    pady=12,
+    cursor="hand2",
+    command=checkout
+)
+checkout_btn.pack(pady=20)
+
+# PRODUCT CARDS
+row = 0
+col = 0
+
+for product in products:
+
+    card = tk.Frame(
+        products_frame,
+        bg=CARD,
+        width=220,
+        height=180,
+        highlightbackground="#E5E7EB",
+        highlightthickness=1
+    )
+
+    card.grid(
+        row=row,
+        column=col,
+        padx=15,
+        pady=15
+    )
+
+    card.grid_propagate(False)
+
+    icon = tk.Label(
+        card,
+        text="🍔",
+        bg=CARD,
+        font=("Segoe UI Emoji", 35)
+    )
+    icon.pack(pady=(15, 5))
+
+    name = tk.Label(
+        card,
+        text=product["name"],
+        bg=CARD,
+        fg=TEXT,
+        font=("Segoe UI", 14, "bold")
+    )
+    name.pack()
+
+    price = tk.Label(
+        card,
+        text=f"₱{product['price']}",
+        bg=CARD,
+        fg=LIGHT,
+        font=("Segoe UI", 12)
+    )
+    price.pack(pady=5)
+
+    add_btn = tk.Button(
+        card,
+        text="Add to Cart",
+        bg=PRIMARY,
+        fg="white",
+        relief="flat",
+        font=("Segoe UI", 11, "bold"),
+        padx=15,
+        pady=8,
+        cursor="hand2",
+        command=lambda p=product: add_to_cart(p)
+    )
+
+    add_btn.pack(pady=15)
+
+    col += 1
+
+    if col == 3:
+        col = 0
+        row += 1
+
+root.mainloop()
